6e513a3ce26507239ef159793ba77add4df0fc4b
Moises lopes ferreira <judeudemolidor@hotmail.com>
2019-02-14 18:58:51 -0200

    Add estrutura sass, fonts e configurações webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const path = require("path");
 const withSass = require("@zeit/next-sass");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const withFonts = require("next-fonts");
 
+module.exports = withFonts({
+  assetPrefix: "https://example.com",
+  webpack(config, options) {
+    return config;
+  }
+});
 const nextConfig = withSass({
   webpack(config, options) {
     config.module.rules.push({
@@ -12,6 +19,18 @@ const nextConfig = withSass({
       type: "javascript/auto",
       test: /\.modernizrrc(\.json)?$/,
       use: ["expose-loader?Modernizr", "modernizr-loader", "json-loader"]
+    });
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          publicPath: "./",
+          outputPath: "static/",
+          name: "[name].[ext]"
+        }
+      }
     });
     config.resolve = {
       alias: {
